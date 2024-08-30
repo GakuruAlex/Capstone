077aa1d1e600ae553c5e81201d339bf15ab73400
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-30 20:50:12 +0300

    tracer 0 for lanes but reset for car movement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
-
+import time
+from random import choice
+from car import Car
+COLORS = ["red", "yellow", "orange", "green", "blue", "black", "magenta"]
 def draw_lanes(turtle, y_cor):
     turtle.pendown()
     turtle.goto(x= 370, y=y_cor)
@@ -9,9 +12,11 @@ def draw_lanes(turtle, y_cor):
 def main() -> None:
     screen = Screen()
     turtle = Turtle()
+
     screen.setup(width= 800, height = 400)
     screen.title("Capstone Game")
     screen.bgcolor("white")
+    screen.tracer(0)
     turtle.penup()
     turtle.goto(-370, -140)
     turtle.color("black")
@@ -20,8 +25,13 @@ def main() -> None:
         draw_lanes(turtle, y_cor)
         y_cor += 20
         turtle.goto(-370, y_cor)
+    turtle.hideturtle()
     game_is_on = True
+    screen.tracer(1)
+    car = Car(location=(370, -130), colour= choice(COLORS), rate="slow" )
     while game_is_on:
+        time.sleep(1)
+        car.move((-370, -130))
         screen.update()
 
 
