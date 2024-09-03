8ee713194131ed8774a72157ff05b6882c3f1cef
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-03 11:57:52 +0300

    change score to level

--- scoreboard.py
+++ scoreboard.py
@@ -5,14 +5,14 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.level = 1
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.display_score()
     def display_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font= FONT)
+        self.write(f"Level: {self.level}", align="center", font= FONT)
     def increase_score(self):
-        self.score += 1
+        self.level += 1
         self.display_score()
