2941b528027d40017af095c51c0a4e245502e276
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-03 09:55:41 +0300

    player behavior

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    screen.listen()
+    screen.onkey(player.move, "w")
