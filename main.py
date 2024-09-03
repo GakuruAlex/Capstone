d38848a1c587aaffd62614b4da4d3d49702d632f
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-03 09:46:53 +0300

    back to drawing board

--- main.py
+++ main.py
@@ -1,56 +1,14 @@
-from turtle import Screen, Turtle
 import time
-from random import choice
-from car import Car
+from turtle import Screen
 from player import Player
-COLORS = ["red", "yellow", "orange", "green", "blue", "black", "magenta"]
-def draw_lanes(turtle, y_cor):
-    turtle.pendown()
-    turtle.goto(x= 370, y=y_cor)
-    turtle.penup()
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 
-def main() -> None:
-    screen = Screen()
-    turtle = Turtle()
-
-    screen.setup(width= 800, height = 400)
-    screen.title("Capstone Game")
-    screen.bgcolor("white")
-    screen.tracer(0)
-    turtle.penup()
-    turtle.goto(-370, -140)
-    turtle.color("black")
-    y_cor = turtle.ycor()
-    for _ in range(0, 15):
-        draw_lanes(turtle, y_cor)
-        y_cor += 20
-        turtle.goto(-370, y_cor)
-    turtle.hideturtle()
-    game_is_on = True
-
-    player = Player()
-    while game_is_on:
-        screen.tracer(0)
-        car = Car(location=(370, -130), colour= choice(COLORS), rate="slowest" )
-        screen.listen()
-        screen.update()
-        time.sleep(1)
-        screen.onkey(player.move_up, "Up")
-        screen.tracer(1)
-        car.move((-370, -130))
-        if car.xcor() < -350:
-            car.clear()
-            screen.update()
-        if player.ycor() > 140:
-            game_is_on = False
-            print(player.ycor())
-
-
-
-
-    screen.exitonclick()
-
-
-if __name__ == "__main__":
-    main()
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
--- player.py
+++ player.py
@@ -1,15 +1,7 @@
-from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
 
-class Player(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.color("green")
-        self.shape("turtle")
-        self.shapesize(stretch_wid=0.7, stretch_len=0.7)
-        self.speed("fast")
-        self.penup()
-        self.setheading(90)
-        self.goto(0, -150)
-    
-    def move_up(self):
-        self.forward(10)
+
+class Player:
+    pass
