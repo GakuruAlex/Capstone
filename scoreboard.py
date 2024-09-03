15efc42da57a14f4667a40e8955e24939243e21d
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-03 11:22:01 +0300

    create and update scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,18 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.display_score()
+    def display_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font= FONT)
+    def increase_score(self):
+        self.score += 1
+        self.display_score()
