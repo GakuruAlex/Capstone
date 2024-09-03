9e47aec0603ff728c05081df34d4a0bd336487ba
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-03 10:09:57 +0300

    car behavior

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+from random import choice, randint
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.cars = []
+        self.generate_cars()
+    def generate_car(self):
+        car = Turtle("square")
+        car.shapesize(stretch_len=2, stretch_wid=4)
+        car.penup()
+        car.color(choice(COLORS))
+        rand_y = randint(-250, 250)
+        car.goto(300, rand_y)
+        self.cars.append(car)
+    def move_cars(self):
+        for car in self.cars:
+            car.backwards(STARTING_MOVE_DISTANCE)
+
--- main.py
+++ main.py
@@ -8,10 +8,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+car_manager = CarManager()
 
 game_is_on = True
+counter = 0
 while game_is_on:
+    counter += 1
     time.sleep(0.1)
     screen.update()
     screen.listen()
     screen.onkey(player.move, "w")
+    car_manager.generate_car()
